3244859f7c1d3c68563147240f82e86f2b5ae820
Suhwan Park <suhwpark@c4r1s1.42seoul.kr>
2023-05-01 16:03:55 +0900

    all mission clear

--- parksooo/level2/day1/33.py
+++ parksooo/level2/day1/33.py
@@ -2,7 +2,7 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        s ,e= 0 ,len(nums) - 1
+        s ,e = 0 ,len(nums) - 1
         while s <= e:
             mid = (s + e) // 2
             if nums[mid] == target:
@@ -20,5 +20,5 @@ class Solution:
         return -1
 
 test = Solution()
-# print(test.search([4,5,6,7,0,1,2], 0))
+print(test.search([4,5,6,7,0,1,2], 0))
 print(test.search([1, 3], 3))
--- parksooo/level2/day1/74.py
+++ parksooo/level2/day1/74.py
@@ -8,5 +8,25 @@ class Solution:
 					return True
 		return False
 
+class Solution2:
+	def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+		m = len(matrix)
+		n = len(matrix[0])
+		s = 0
+		e = (m * n) - 1
+		while s <= e:
+			mid = s + (e - s) // 2
+			r = mid // n
+			l = mid % n
+			if matrix[r][l] == target:
+				return True
+			elif matrix[r][l] > target:
+				e = mid - 1
+			else : 
+				s = mid + 1
+		return False
+
 test = Solution()
+test2 = Solution2()
+print(test2.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
 print(test.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))
