28e49c2857823047fd87e5b42df4457bc729738f
Suhwan Park <suhwpark@c4r1s6.42seoul.kr>
2023-05-10 15:20:09 +0900

    47 mission clear more faster slove

--- parksooo/level2/day10/47.py
+++ parksooo/level2/day10/47.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import List
 from itertools import permutations
 class Solution:
@@ -8,5 +9,28 @@ class Solution:
             if i not in res:
                 res.append(i)
         return res
-test = Solution()
+# 제가 푼 방식은 내장함수를 사용하여 순열을 만들어서 중복 없이 append해서 넣어 준다
+# 하지만 시간이 너무 오래걸림  
+class Solution1:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(sub, counter):
+            if len(sub) == len(nums):
+                res.append(list(sub))
+                return
+            for n in counter:
+                if counter[n] > 0:
+                    sub.append(n)
+                    counter[n] -= 1
+                    dfs(sub, counter)
+                    sub.pop()
+                    counter[n] += 1
+        dfs([], Counter(nums))
+        return res
+# 참조한 방식 : 백트래킹을 사용한다
+# 카운터를 사용하여, nums의 길이만큼 sub이라는 [] 에 추가 되면, 그것을 res에 넣어준다. 
+# 중복은 sub.pop을 통해서 삭제해준다
+
+
+test = Solution1()
 print(test.permuteUnique([1,1,2]))
