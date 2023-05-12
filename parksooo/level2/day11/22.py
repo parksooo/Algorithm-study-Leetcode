64abe34972fb0957443a69c6dbb1d651f256640f
Suhwan Park <suhwpark@c4r1s1.42seoul.kr>
2023-05-12 11:01:26 +0900

    22 mission clear

---
+++ parksooo/level2/day11/22.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def dfs(combi_str, left, right):
+            if len(combi_str) == 2 * n:
+                res.append("".join(combi_str))
+                return
+            if left < n :
+                combi_str.append("(")
+                dfs(combi_str, left + 1, right)
+                combi_str.pop()
+            if left > right :
+                combi_str.append(")")
+                dfs(combi_str, left, right + 1)
+                combi_str.pop()
+        dfs([], 0, 0)
+        return res
+test = Solution()
+print(test.generateParenthesis(3))
