745a25a8a9d3bee751e9e347f4ec5de69019219d
Suhwan Park <suhwpark@c4r2s2.42seoul.kr>
2023-05-15 19:16:37 +0900

    day12 mission all clear

--- parksooo/level2/day12/55.py
+++ parksooo/level2/day12/55.py
@@ -3,9 +3,14 @@ from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         l = len(nums)
-        dp = [False] * l
-        dp[0] = True
+        # dp = [0 for i in range(l)]
+        jmp_cnt = 0
+        for i in range(l):
+            if jmp_cnt < i: # 현지점을 갈수 있는지 판단하는 경우
+                return False
+            jmp_cnt = max(nums[i] + i, jmp_cnt)
+        return True
 
-
-
-a = [5, 3, 2, 1, 2, 0]
+test = Solution()
+a = [1, 3, 2, 1, 1, 1, 1]
+print(test.canJump(a))
